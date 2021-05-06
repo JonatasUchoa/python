a49fd87474cf5da257d160e5a247117df99a2f99
jônatas Uchôa Zeferino <jonatasu_taz@hotmail.com>
2021-05-06 19:59:49 -0400

    progama para votação

---
+++ votacao.py
@@ -0,0 +1,20 @@
+#eleção com 3 candidatos deve pedir o número de eletores seu voto e mostrar o resultado parcial para cada eleitor que votar
+
+carlao = 0
+pedrao = 0
+josefa = 0
+numeroEleitores = int(input("digite o número de total de eleitores:\n"))
+voto = 0
+for i in range(0,numeroEleitores,1):
+    voto = int(input("VOTE COM CONSCIÊNCIA!!\ndigite 1 para votar no Carlao, 2 para o Pedrao e 3 para votar na Josefa:\n"))
+    if voto == 1:
+        carlao += 1
+        print("votos: Carlão:",carlao,"| Pedrão:",pedrao, "| Josefa:",josefa)
+    elif voto == 2:
+        pedrao += 1
+        print("votos: Carlão:",carlao,"| Pedrão:",pedrao, "| Josefa:",josefa)
+    elif voto == 3:
+        josefa +=1
+        print("votos: Carlão:",carlao,"| Pedrão:",pedrao, "| Josefa:",josefa)
+    else:
+        print("digite um número válido")
